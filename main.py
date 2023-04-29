e87c8a8e2a8e7e056f05e25cc7e425ef9da54f46
TakahiroTada <takass0119@yahoo.co.jp>
2023-04-30 00:58:42 +0900

    リファクタリング

--- main.py
+++ main.py
@@ -1,61 +1,21 @@
-import datetime
-import json
-from fastapi import FastAPI, Response
-from pydantic import BaseModel
+from fastapi import FastAPI
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
+from models import Base
+from config import load_config
+from routes import get_tweets
 
 app = FastAPI()
-
-class Config(BaseModel):
-    connection_string: str
-
-class Config:
-    def __init__(self, connection_string):
-        self.connection_string = connection_string
-
-# configの読み込み
-with open("config.json") as f:
-    loaded_config = json.load(f)
-    config = Config(**loaded_config)
-
-database_url = config.connection_string
-engine = create_engine(database_url)
-
-Base = declarative_base()
-
-class ServiceUsers(Base):
-    __tablename__ = "service_users"
-
-    service_user_id = Column(String(255), primary_key=True)
-    name = Column(String(255))
-    mailaddress = Column(String(255))
-    password = Column(String(255))
-    deleted = Column(Integer)
 
 # テーブルの作成（必要に応じて）
 # Base.metadata.create_all(engine)
 
+config = load_config()
+database_url = config.connection_string
+engine = create_engine(database_url)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 @app.get("/test/tweets")
-def get_tweets():
-
-    session = SessionLocal()
-    service_users = session.query(ServiceUsers).all()
-    session.close()
-
-    for service_user in service_users:
-        print(f"ID: {service_user.service_user_id}, Name: {service_user.name}, MAIL: {service_user.mailaddress}")
-
-    return {"tweets": "tweets"}
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def get_all_tweets():
+    return get_tweets(engine)
