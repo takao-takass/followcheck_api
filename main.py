6c525ade54ff6060e54fc3b5ca1c72ea10e8d86f
TakahiroTada <takass0119@yahoo.co.jp>
2023-04-30 00:42:12 +0900

    リファクタリング

--- main.py
+++ main.py
@@ -9,44 +9,42 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Config(BaseModel):
+    connection_string: str
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+class Config:
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+
+# configの読み込み
+with open("config.json") as f:
+    loaded_config = json.load(f)
+    config = Config(**loaded_config)
+
+database_url = config.connection_string
+engine = create_engine(database_url)
+
+Base = declarative_base()
+
+class ServiceUsers(Base):
+    __tablename__ = "service_users"
+
+    service_user_id = Column(String(255), primary_key=True)
+    name = Column(String(255))
+    mailaddress = Column(String(255))
+    password = Column(String(255))
+    deleted = Column(Integer)
+
+# テーブルの作成（必要に応じて）
+# Base.metadata.create_all(engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 @app.get("/test/tweets")
 def get_tweets():
 
-    config = None
-    with open("config.json") as f:
-        loaded_config = json.load(f)
-        config = Config(**loaded_config)
-
-    if config is None:
-        return Response(status_code=400)
-    
-    database_url = config.connection_string
-    engine = create_engine(database_url)
-
-    Base = declarative_base()
-    class ServiceUsers(Base):
-
-        __tablename__ = "service_users"
-
-        service_user_id = Column(String(255), primary_key=True)
-        name = Column(String(255))
-        mailaddress = Column(String(255))
-        password = Column(String(255))
-        deleted = Column(Integer)
-
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     session = SessionLocal()
-
     service_users = session.query(ServiceUsers).all()
-
     session.close()
 
     for service_user in service_users:
@@ -54,6 +52,10 @@ def get_tweets():
 
     return {"tweets": "tweets"}
 
-# 接続文字列のモデルクラスを定義します。
-class Config(BaseModel):
-    connection_string: str
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
