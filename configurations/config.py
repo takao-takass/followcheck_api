fcb991d859733c0d473c055beb75e77d711052b0
TakahiroTada <takass0119@yahoo.co.jp>
2023-04-30 20:42:47 +0900

    リファクタリング

--- configurations/config.py
+++ configurations/config.py
@@ -1,10 +1,11 @@
 import json
+from typing import Optional
 
 class Config:
-    def __init__(self, connection_string):
+    def __init__(self, connection_string: str):
         self.connection_string = connection_string
 
-def load_config(filename="config.json"):
+def load_config(filename: Optional[str] = "config.json") -> Config:
     with open(filename) as f:
         loaded_config = json.load(f)
         config = Config(**loaded_config)
