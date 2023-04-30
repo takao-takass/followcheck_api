05eaa546a42607ac216ce3e278167734e132b712
TakahiroTada <takass0119@yahoo.co.jp>
2023-04-30 20:38:33 +0900

    リファクタリング

--- logics/user.py
+++ logics/user.py
@@ -1,23 +1,16 @@
 from sqlalchemy.orm import sessionmaker
 from databases.service_users import ServiceUsers
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False)
+
 def get_service_users(engine):
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    
-    session = SessionLocal()
+    with SessionLocal(bind=engine) as session:
+        service_users = session.query(ServiceUsers).all()
 
-    service_users = session.query(ServiceUsers).all()
-
-    session.close()
-
-    service_users = [ServiceUserModel(service_user.service_user_id, service_user.name, service_user.mailaddress) for service_user in service_users]
-
-    return service_users
+    return (ServiceUserModel(service_user.service_user_id, service_user.name, service_user.mailaddress) for service_user in service_users)
 
 class ServiceUserModel:
     def __init__(self, service_user_id, name, mailaddress):
         self.service_user_id = service_user_id
         self.name = name
         self.mailaddress = mailaddress
-
-
